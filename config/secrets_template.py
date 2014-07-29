b9939db48a7e078649c48514064a78997c9558cc
Daniel Kuntz <dkuntz417@gmail.com>
2014-07-29 17:57:21 -0600

    Update secrets_template.py

--- config/secrets_template.py
+++ config/secrets_template.py
@@ -3,7 +3,7 @@ Secret settings local to this machine.
 """
 SECRET_KEY = '{{secret_key}}'
 DEBUG = {{debug}}
-TEMPLATE_DEBUG = '{{template_debug}}'
+TEMPLATE_DEBUG = {{template_debug}}
 DATABASE_NAME = '{{django_db_name}}'
 DATABASE_USER = '{{django_db_user}}'
 DATABASE_PASSWORD = '{{django_db_pwd}}'
